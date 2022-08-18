a62aa08a5823e6d23509602cc80a72de758b264b
Nicolas Bueno <nicolasbrian202@gmail.com>
2022-08-17 22:47:39 -0300

    DAO

--- src/br/com/vendasnb/model/ModelCargo.java
+++ src/br/com/vendasnb/model/ModelCargo.java
@@ -10,4 +10,50 @@ package br.com.vendasnb.model;
  */
 public class ModelCargo {
     
+    private int idCargo;
+    private String descricao;
+    private Double salario;
+
+    /**
+     * @return the idCargo
+     */
+    public int getIdCargo() {
+        return idCargo;
+    }
+
+    /**
+     * @param idCargo the idCargo to set
+     */
+    public void setIdCargo(int idCargo) {
+        this.idCargo = idCargo;
+    }
+
+    /**
+     * @return the descricao
+     */
+    public String getDescricao() {
+        return descricao;
+    }
+
+    /**
+     * @param descricao the descricao to set
+     */
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    /**
+     * @return the salario
+     */
+    public Double getSalario() {
+        return salario;
+    }
+
+    /**
+     * @param salario the salario to set
+     */
+    public void setSalario(Double salario) {
+        this.salario = salario;
+    }
+    
 }
